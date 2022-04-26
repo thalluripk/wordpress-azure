eb4e8472e080737d478bd022cd89990d119ff069
Praveen Thalluri <Praveen.Thalluri@rac.com.au>
2022-04-26 22:32:05 +0800

    header changes for 2021 theme

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -18,6 +18,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
+	<script>alert("this is test");
+			</script>
 </head>
 
 <body <?php body_class(); ?>>
